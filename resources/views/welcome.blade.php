564525ee020bde9a09426b23bfa7ac0f75ad3b1a
thamo2020 <thamodya01@gmail.com>
2021-01-05 01:20:39 +0530

    home page-navigation bar completion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,63 +8,25 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{asset('/css/homestyle.css')}}" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+       
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+
+    <section>
+
+    
+        
+        <header>
+
+        
+
+            <a href="#" class="logo">Steth</a>
+            <div class="toggle" onclick="menuToggle();"></div>
+
+            <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,22 +41,41 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            
         </div>
+
+
+        </header>
+    
+        <div class="glass"></div>
+        <div class="content">
+            <h2>Hapiness is..<br><span>Healthfull Life</span></h2>
+    
+            <h2 class="h2">ආරෝග්‍යා පරමා ලාබා</h2>
+        </div>
+        <video src="/images/production ID_4444268.mp4 " autoplay muted loop></video>
+    
+        <div class="navigation">
+            <li style="--i:1;"><a href="#">Home</a></li>
+            <li style="--i:2;"><a href="#">How it works</a></li>
+            <li style="--i:3;"><a href="#">About Us</a></li>
+            <li style="--i:4;"><a href="#">Contact Us</a></li>
+    
+        </div>
+    
+        </section>
+    
+    
+        <script type="text/javascript">
+            function menuToggle(){
+             const toggleMenu = document.querySelector('.toggle');
+             const  section= document.querySelector('section');
+             toggleMenu.classList.toggle('active');   
+             section   .classList.toggle('active');   
+            }
+    
+        </script>
+
+        
     </body>
 </html>
